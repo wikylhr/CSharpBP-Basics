b7ac3c2ff734803937371c7462d14f9f906b7ac1
Waqas Dell-Workstation <waqas.lhr2@gmail.com>
2018-10-29 02:02:38 +0500

    Made some minor changes following the online videos.

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    /// Manages products being held in inventory.
+    /// </summary>
+    public class Product
+    {
+        public Product()
+        {
+            Console.WriteLine("A new instance of 'Product' class has been created.");
+        }
+        
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        private string description;
+
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        private int productId;
+
+        public int ProductID
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string SayHello()
+        {
+            return $"Hello {productName} ({productId}): {description}"; 
+        }
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            Product currentProduct = new Product();
+            currentProduct.ProductID = 1;
+            currentProduct.ProductName = "Pizza";
+            currentProduct.Description = "A tasty mushroom and olives pizza.";
+            string expected = "Hello Pizza (1): A tasty mushroom and olives pizza.";
+
+            //Act
+            string actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
